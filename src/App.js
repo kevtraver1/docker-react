37d71d4c221a5740389259bbf166da836c744a4b
Travers <ktravers@cbs.com>
2018-09-20 11:55:58 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hi there Kevin!!!
+          Testing Git Branches!!!
         </p>
       </div>
     );
